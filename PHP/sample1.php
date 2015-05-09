63bc8714d49971fb985d3d2e97c57dc1ab3e0606
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-05-09 13:52:33 +0300

    Adds a sample file

---
+++ PHP/sample1.php
@@ -0,0 +1,27 @@
+<?php
+	/*
+	 * This sample shows how to modify a SELECT query by adding a WHERE clause
+	 */
+	 echo '<pre>';
+	 include_once(__DIR__.'/lib/SqlParser.class.php');
+	 
+	 $query='SELECT customers.*, users.email
+			FROM customers LEFT JOIN users
+				USING(id_user)
+			ORDER BY customers.first_name ASC
+			LIMIT 10';
+	 
+	 $parser=new \alib\utils\SqlParser();
+	 
+	 $parser->parse($query);
+	 $parser->appendWhere('customers.first_name="John"'); 
+	 
+	 $newQuery=$parser->rebuildSource();
+	 
+	 
+	 
+	 echo 'Query: '.$query;
+	 echo '<hr>';
+	 echo 'Modified query: '.$newQuery;
+	 
+	 
