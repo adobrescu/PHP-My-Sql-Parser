e512e7c744873c822930b80361460b237dc8b25c
Adi Dobrescu <adobrescu@insolitservices.ro>
2015-05-15 14:45:22 +0300

    Sample script: modifying INSERTs

---
+++ PHP/sample8.php
@@ -0,0 +1,58 @@
+<?php
+	/*
+	 * This sample shows how to get field names from the SELECT in a SELECT and from sub-queries
+	 */
+	
+	 include_once(__DIR__.'/lib/SqlParser.class.php');
+	 
+	 
+	$query='INSERT INTO tbl (field1, field2) VALUES (\'a\', \'b\')';
+	 	 
+	$parser=new \alib\utils\SqlParser();
+	echo '<pre>';
+	
+	$parser->parse($query);
+	$parser->addInsertColumns(
+			array('field3', 'field4'),
+			array('value3', '"value4"')
+			);
+	echo $parser->rebuildSource().'<br>';
+	
+	//////////////////////////////////////////////////
+	$query='INSERT INTO tbl VALUES (\'a\', \'b\')';
+	
+	$parser->parse($query);
+	$parser->addInsertColumns(
+			array('field3', 'field4'),
+			array('value3', '"value4"')
+			);
+	echo $parser->rebuildSource().'<br>';
+	
+	//////////////////////////////////////////////////
+	$query='INSERT INTO tbl (field1, field2) (SELECT 1, 2 FROM tbl)';
+	
+	$parser->parse($query);
+	$parser->addInsertColumns(
+			array('field3', 'field4'),
+			array('value3', '"value4"')
+			);
+	echo $parser->rebuildSource().'<br>';
+	
+	//////////////////////////////////////////////////
+	$query='INSERT INTO tbl (SELECT 1, 2 FROM tbl)';
+	
+	$parser->parse($query);
+	$parser->addInsertColumns(
+			array('field3', 'field4'),
+			array('value3', '"value4"')
+			);
+	echo $parser->rebuildSource().'<br>';
+	
+	return;
+	$query='INSERT INTO tbl (a,b) SELECT \'c\'';
+	$parser->parse($query); 
+	
+	$query='INSERT INTO tbl ((SELECT c)) ';
+	$parser->parse($query);
+	
+	echo $parser->rebuildSource();
